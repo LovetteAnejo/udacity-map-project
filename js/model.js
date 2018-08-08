3ae8896ea3dc299ad2bba14f6e055d9a852187bd
LovetteAnejo <34012835+LovetteAnejo@users.noreply.github.com>
2018-08-08 18:31:17 +0100

    Update model.js

--- js/model.js
+++ js/model.js
@@ -1,9 +1,10 @@
 // This is the model
-var markers = [];
+
+// This the raw location coordinate
 var myLocations = [
-		{title: "Gdss Wuse II", location: {lat:9.079468, lng: 7.481755}},
-		{title: "Home", location: {lat:9.079224, lng: 7.501334}},
+		{title: "Gudu", location: {lat: 9.008361, lng: 7.477578}},
+		{title: "Shoprite Apo", location: {lat:8.985319,lng: 7.493129}},
 		{title: "Millenium Park", location: {lat:9.071078, lng: 7.496232}},
 		{title: "Holy Trinity Church", location: {lat:9.082755, lng: 7.487082}},
-		{title: "Malanm Shop", location: {lat:9.075868, lng: 7.500234}}	
-	];
+		{title: "Zone E", location: {lat:9.004951, lng: 7.482301}}	
+];
